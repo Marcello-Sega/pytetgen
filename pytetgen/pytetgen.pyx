7351c5d3443c34b1a0bc56fa639e644d4d0752a2
Marion Artigaut <77725213+Marion-Artigaut@users.noreply.github.com>
2023-10-02 12:03:15 +1000

    Update pytetgen.pyx

--- pytetgen/pytetgen.pyx
+++ pytetgen/pytetgen.pyx
@@ -1,3 +1,4 @@
+# cython: language_level=2
 # -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding: utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 # distutils: language=c++
